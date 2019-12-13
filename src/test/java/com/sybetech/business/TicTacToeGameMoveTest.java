ddb462f3c2961751ed7ccd9ee4cf1981e7428eac
Jan Polzer <janman74@gmail.com>
2019-12-12 23:03:56 -0600

    Add unit tests

--- src/test/java/com/sybetech/business/TicTacToeGameMoveTest.java
+++ src/test/java/com/sybetech/business/TicTacToeGameMoveTest.java
@@ -22,64 +22,69 @@ public class TicTacToeGameMoveTest {
     private final char player = 'X';
 
     // https://github.com/junit-team/junit4/wiki/rules (JUnit interceptors)
-//    @Rule
+    @Rule
     public final TestName name = new TestName();
 
-//    @BeforeClass
+    @BeforeClass
     public static void beforeClass() {
         // called once on class initialization time
     }
 
-//    @AfterClass
+    @AfterClass
     public static void afterClass() {
         // called once after all methods are executed
     }
 
-//    @Before
+    @Before
     public void before() {
         // called before each method execution
         // init a move
+    	move = new TicTacToeGameMove(id, player, x, y);
+
     }
 
-//    @After
+    @After
     public void after() {
         // called after each method execution
+    	move = null;
     }
 
     // check id is set after instantiation
-    //@Test
+    @Test
     public void whenInstantiated_ThenIdIsSet() {
-
+    	assertEquals("Asserting that id is set in constructor", id, move.getId());
     }
 
     // check x is set after instantiation
-    //@Test
+    @Test
     public void whenInstantiated_ThenXIsSet() {
-
+    	assertEquals("Asserting that X is set after move", x, move.getX());
     }
 
     // check y is set after instantiation
-    //@Test
+    @Test
     public void whenInstantiated_ThenYIsSet() {
-
+    	assertEquals("Asserting that Y is set after move", y, move.getY());
     }
 
     // check player is set after instantiation
-    //@Test
+    @Test
     public void whenInstantiated_ThenPlayerIsSet() {
-
+    	assertEquals("Asserting that player is set in constructor", player, move.getPlayer());
     }
 
     // check allowedChars are O and X after instantiation
-    //@Test
+    @Test
     public void whenInstantiated_ThenAllowedCharsAreOandX() {
-
+    	List<Character> expectedCharacters = Arrays.asList('X');
+    	assertTrue(move.getAllowedChars().containsAll(expectedCharacters));
+    	assertEquals(2, move.getAllowedChars().size());
     }
 
     // check Rule TestName.getMethodName
-    //@Test
+    @Test
     public void whenGetMethodNameOfTestNameRuleInvoked_ThenReturnNameOfThisTest() {
-
+    	assertEquals("whenGetMethodNameOfTestNameRuleInvoked_ThenReturnNameOfThisTest", name.getMethodName());
     }
 
 }
